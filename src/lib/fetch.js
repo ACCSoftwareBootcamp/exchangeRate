1c91c7ef2ea0537a1f37d710739959e9e2217df1
Stephen Apineru <sa2013@yahoo.com>
2022-07-09 14:10:58 -0500

    revert last commit for delay

--- src/lib/fetch.js
+++ src/lib/fetch.js
@@ -10,6 +10,7 @@ const sleep = (time) => new Promise((res) => setTimeout(res, time));
 export async function fetch(url) {
   const [, search = ""] = url.split("?");
   const params = new URLSearchParams(search);
+  //reverted back to 400
   await sleep(400);
   // const symbols = new Set((params.get("symbols") || "").split(","));
   const base = params.get("base");
